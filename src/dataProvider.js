d1f43c7c74a41ca7adf9f4e4971b131f78cabea7
KuroPebbles <darkchoaskeeper@gmail.com>
2025-02-09 13:01:42 -0500

    Homescreen changes "Still needs CSS changes"

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,8 +1,8 @@
 export function getLeaderboardData() {
     return [
-     {Place: 1, Name: "Joe", Score: 45},
-     {Place: 2, Name: "Anna", Score: 40},
-     {Place: 3, Name: "Alex", Score: 35},
-     {Place: 4, Name: "Kate", Score: 30}
+     {Place: 1, Pfp: "../assets/defaultpfp.png", Name: "Joe", Score: 45},
+     {Place: 2, Pfp: '../assets/defaultpfp.png', Name: "Anna", Score: 40},
+     {Place: 3, Pfp: '../assets/defaultpfp.png', Name: "Alex", Score: 35},
+     {Place: 4, Pfp: '../assets/defaultpfp.png', Name: "Kate", Score: 30}
     ]; 
  };
