e3be18407bf2c9939c45f6fda6b77d47ad880846
SunCh1p <blaha.s.connor@gmail.com>
2025-02-18 20:39:13 -0500

    Added initial code for redis

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,19 @@ import socket from './socket';
 
 import Home from './components/Home.jsx';
 import Leaderboard from './components/Leaderboard.jsx';
-import Login from './components/login.jsx';
-import Room from './components/Room.jsx';
 import PlayScreen from './components/PlayScreen.jsx';
-import Test from './components/Test.jsx';
 import CreateGame from './components/CreateGame.jsx'
 import MenuSettings from './components/MenuSettings.jsx';
 import {getAllPlayers} from './dataProvider.js';
+
+//Components used back backend for testing
+import TestHome from './components/testcomponents/TestHome.jsx';
+import TestJoin from './components/testcomponents/TestJoin.jsx';
+import TestCreate from './components/testcomponents/TestCreate.jsx';
+import TestLeaderboard from './components/testcomponents/TestLeaderboard.jsx';
+import TestGame from './components/testcomponents/TestGame.jsx';
+
+
 
 
 
@@ -39,9 +45,14 @@ function App() {
           <Route path='/play' element={<PlayScreen/>}/>
           <Route path='/leaderboard' element={<Leaderboard players={game.allPlayers}/>}/>
           <Route path='/MenuSettings' element={<MenuSettings/>}/>
-          <Route path='/Test' element={<Test/>}/>
-          <Route path='/TestLogin' element={<Login/>}/>
           <Route path='/create' element={<CreateGame game={game} setGame={setGame}/>}/>
+          
+          {/*Routes for test components for back end*/}
+          <Route path='/TestHome' element={<TestHome/>}/>
+          <Route path='/TestJoin' element={<TestJoin/>}/>
+          <Route path='/TestCreate' element={<TestCreate/>}/>
+          <Route path='/TestLeaderboard' element={<TestLeaderboard/>}/>
+          <Route path='/TestGame' element={<TestGame/>}/>
         </Routes>
       </div>
     </main>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -26,7 +26,7 @@ const Home = props => {
     }
 
     const BackEndClick = () => {
-        navigate("/TestLogin")
+        navigate("/TestHome")
     }
     
 
