cc54375656836fea8997f715f41fbda83b10a0fb
SunCh1p <blaha.s.connor@gmail.com>
2025-02-19 21:08:23 -0500

    changes

--- client/src/utils/imagePredict.js
+++ client/src/utils/imagePredict.js
@@ -4,10 +4,11 @@ import * as tf from '@tensorflow/tfjs';
 const labels = ['shoe','no_item']; // Add all your labels here
 
 // Function to load and predict
+let model;
 export async function loadModelAndPredict(imageData) {
-    // Load the model
-    const model = await tf.loadLayersModel('https://teachablemachine.withgoogle.com/models/-ry1ZS2sh/model.json');
-
+    if(!model){
+        model = await tf.loadLayersModel('https://teachablemachine.withgoogle.com/models/-ry1ZS2sh/model.json');
+    }
     // Preprocess the image
     const image = tf.browser.fromPixels(imageData);
     const resizedImage = tf.image.resizeBilinear(image, [224, 224]);
@@ -20,6 +21,9 @@ export async function loadModelAndPredict(imageData) {
 
     // Get the index of the highest confidence prediction
     const maxIndex = predictionArray[0].indexOf(Math.max(...predictionArray[0]));
+
+    // Cleanup tensors to free memory
+    tf.dispose([image, resizedImage, normalizedImage, batchedImage, predictions]);
 
     // Return the label corresponding to the highest confidence prediction
     return labels[maxIndex];
