f2314c2b3df2d23fb33fb87ccb46c1b39a3750b9
KuroPebbles <darkchoaskeeper@gmail.com>
2025-03-18 21:42:10 +0000

    fix2

---
+++ client/src/ThemeContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState, useContext, useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light'); // Default theme
+
+  const contextValue = useMemo(() => {
+    return {
+      theme,
+      setTheme,
+    };
+  }, [theme, setTheme]);
+
+  return (
+    <ThemeContext.Provider value={contextValue}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
