10638f8756b41fc4cad0a1818051e2a92fbb8e38
dannyhido <dannyhido@gmail.com>
2025-04-15 13:50:15 -0400

    fixes

--- server/app.py
+++ server/app.py
@@ -395,6 +395,7 @@ def handle_submit(data):
   if submitItem == items[score%len(items)]:
     #update the score
     score += 1
+    emit('point')
     db_set_user_score(cursor, request.sid, score)
     print(score)
 
@@ -402,7 +403,6 @@ def handle_submit(data):
     game_mode = data.get('mode')
     if isinstance(game_mode, list):
       game_mode = game_mode[0]  # Extract the first element if it's a list
-    print(game_mode)
 
     #check if victory
     if score == 5 and game_mode == "ItemRace":
@@ -413,7 +413,7 @@ def handle_submit(data):
       startTime = db_get_room_time(cursor, roomCode)[0]
       endTime = time.time()
       finalTime = round(endTime - startTime, 4)
-      save_scores(roomCode, finalTime)
+      save_scores(roomCode, finalTime, game_mode)
       emit('winner', {'message': f"{username}", 'time': f"{finalTime}"}, room=roomCode)
   #get new game state
   gameState = db_get_game_state(cursor, roomCode)
@@ -429,25 +429,25 @@ def handle_get_leaderboard_data(data):
     print("formatted data: ", formatted_scores)
     emit('leaderboard data', formatted_scores, room=request.sid)
 
-def save_scores(roomCode, finalTime):
+def save_scores(roomCode, finalTime, gameMode):
   print("saving scores")
-  gameMode = db_get_game_mode(roomCode)
+  #gameMode = db_get_game_mode(roomCode)
   place = 1
   users = db_get_users_in_room_by_score(cursor, roomCode)
-  if gameMode == "itemRace":
+  if gameMode == "ItemRace":
     # score == time
     for user in users:
-      save_score(user[2], f"{finalTime} seconds", "itemRace", place)
+      save_score(user[2], f"{finalTime} seconds", "ItemRace", place)
       place += 1
-  elif gameMode == "itemBlitz":
+  elif gameMode == "ItemBlitz":
   # score == time;
       for user in users:
-        save_score(user[2], f"{user[3]} points", "itemBlitz", place)
+        save_score(user[2], f"{user[3]} points", "ItemBlitz", place)
         place += 1
-  elif gameMode == "geoQuest":
+  elif gameMode == "GeoQuest":
   # TODO i think this is first to complete may need to change 
     for user in users:
-        save_score(user[2], f"{finalTime} seconds", "itemBlitz", place)
+        save_score(user[2], f"{finalTime} seconds", "ItemBlitz", place)
         place += 1
   else:
     print(f"Unknown game mode: {gameMode}")
